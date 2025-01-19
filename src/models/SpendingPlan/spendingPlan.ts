f59cee01c68d639812ba5b45000c7b99bb1d7ff8
Black Box <developer.luke.barker3@outlook.com>
2025-01-19 17:49:04 +0000

    Add budgets to spending plan model

--- src/models/SpendingPlan/spendingPlan.ts
+++ src/models/SpendingPlan/spendingPlan.ts
@@ -1,7 +1,9 @@
+import { Budget } from "../Budget/budget";
 import { TransactionItem } from "../common/transactionItem";
 
 export interface SpendingPlan {
     name: string;
     currency: string;
     transactions: TransactionItem[];
+    budgets: Budget[];
 }
